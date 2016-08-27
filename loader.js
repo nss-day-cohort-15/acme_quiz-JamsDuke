b3744601123b25e18e3f7b84a88e9107c4876de2
Jamie Duke <jamsduke@gmail.com>
2016-08-27 13:59:33 -0500

    JSON files loading via promises

--- loader.js
+++ loader.js
@@ -1,9 +1,20 @@
 console.log("loader.js loaded!")
 
 var Acme = (function(acme){
+  var categories
+  var types
+  var products
+
   storeShelf = $(".container")
   storeShelf.prepend(
-    ``)
+    `<div>
+      <select name="" id="">
+        <option value="fireworks" name="category" selected>Fireworks</option>
+        <option value="demotion" name="category">Demolition</option>
+      </select>
+      <button id="displayProducts">Display Products</button>
+    </div>
+    `)
   $("#displayProducts").on("click", function(){
     acme.loadItems()
   })
@@ -14,6 +25,12 @@ var Acme = (function(acme){
       $.getJSON("types.json"),
       $.getJSON("products.json")
       ])
+    .then(function(organizer){
+      categories = organizer[0].categories
+      types = organizer[1].types
+      products = organizer[2].products
+      acme.selectCategory(categories, types, products)
+    })
   }
   return acme
 
