a5cb9349f2232cdbbe7530431d601a961c4570a6
Jamie Duke <jamsduke@gmail.com>
2016-08-27 13:30:12 -0500

    trouble getting JSON to load

--- loader.js
+++ loader.js
@@ -1,5 +1,20 @@
 console.log("loader.js loaded!")
 
 var Acme = (function(acme){
+  storeShelf = $(".container")
+  storeShelf.prepend(
+    ``)
+  $("#displayProducts").on("click", function(){
+    acme.loadJson()
+  })
+
+  acme.loadJson = function(){
+    Promise.all([
+      $.getJSON("categories.json"),
+      $.getJSON("types.json"),
+      $.getJSON("products.json")
+      ])
+  }
+  return acme
 
 })(Acme);
