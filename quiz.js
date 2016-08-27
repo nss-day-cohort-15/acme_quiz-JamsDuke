35d4df7cfced15393663740adb4536f993250973
Jamie Duke <jamsduke@gmail.com>
2016-08-27 12:57:25 -0500

    touched organizer.js for possible future use, created category select function

--- quiz.js
+++ quiz.js
@@ -2,4 +2,15 @@ console.log("quiz.js loaded!")
 
 var Acme = (function(acme){
 
+  // Select Category
+  acme.selectCategory = function (category, type, product){
+    var userSelect = $("#categoryBar").val()
+    if (userSelect === "fireworks"){
+      acme.fireworksSelected(category, type, product)
+    }
+    if (userSelect === "demolition"){
+      acme.demolitionSelected(category, type, product)
+    }
+  }
+
 })(Acme);
